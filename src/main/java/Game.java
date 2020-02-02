8f1c5d42ece6a9789bf5b53e8d60b353d5fbccad
Paul McManus <pmcmanus2021@gmail.com>
2020-02-02 18:02:17 +0000

    Compares 2 players, draw event not implemented

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,34 @@
+import java.util.ArrayList;
+
 public class Game {
+
+    private ArrayList<Player> players;
+    private Player dealer;
+    private Deck deck;
+
+    public Game() {
+        this.players = new ArrayList<Player>();
+        this.dealer = new Player();
+        this.deck = new Deck();
+        this.players.add(dealer);
+    }
+
+    public void addPlayer(Player player){
+        this.players.add(player);
+    }
+
+    public int getPlayerCount() {
+        return this.players.size();
+    }
+
+    //Need to put draw event in.
+    public Player compareHandValues(Player player1, Player player2) {
+           if (player1.getHandValue() > player2.getHandValue()){
+                 return player1;}
+           else { return player2;}
+           }
+
+
 
 }
 
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,3 +1,68 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class GameTest {
 
+    Game game;
+    Card card1;
+    Card card2;
+    Card card3;
+    Card card4;
+    Player player1;
+    Player player2;
+    ArrayList<Player> players;
+
+
+    @Before
+    public void before(){
+        game = new Game();
+        card1 = new Card(SuitType.HEARTS,RankType.ACE);
+        card2 = new Card(SuitType.CLUBS,RankType.EIGHT);
+        card3 = new Card(SuitType.SPADES,RankType.JACK);
+        card4 = new Card(SuitType.DIAMONDS,RankType.FIVE);
+        player1 = new Player();
+        player2 = new Player();
+        players = new ArrayList<Player>();
+    }
+
+    @Test
+    public void canDealMultipleCardsToMultiplePlayers(){
+        player1.addCardToHand(card1);
+        player2.addCardToHand(card2);
+        player1.addCardToHand(card3);
+        player2.addCardToHand(card4);
+        assertEquals(2, player1.getPlayerHandSize());
+        assertEquals(2, player2.getPlayerHandSize());
+    }
+
+    @Test
+    public void canGetMultiplePlayersHandValue(){
+        player1.addCardToHand(card1);
+        player2.addCardToHand(card2);
+        player1.addCardToHand(card3);
+        player2.addCardToHand(card4);
+        assertEquals(11, player1.getHandValue());
+        assertEquals(13, player2.getHandValue());
+    }
+
+    @Test
+    public void canAddPlayersToArrayList() {
+        game.addPlayer(player1);
+        assertEquals(2, game.getPlayerCount());
+    }
+
+    @Test
+    public void canCompareHandValues(){
+        game.addPlayer(player1);
+        game.addPlayer(player2);
+        player1.addCardToHand(card1);
+        player2.addCardToHand(card2);
+        player1.addCardToHand(card3);
+        player2.addCardToHand(card4);
+        assertEquals(player2, game.compareHandValues(player1, player2));
+    }
 }
